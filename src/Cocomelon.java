aceff7e4e3a7846f912e3c29b7dc2a5e24042e99
volkaud <daria.volkau@gmail.com>
2020-11-12 11:21:46 -0500

    class created

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,6 +1,8 @@
 public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("Trying to nail it");
+        System.out.println("i will succeed");
+        // oh yes i will
     }
 }
 
